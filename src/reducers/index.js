ce18bc2309eb7e0d03833e3a6ff8c988e0e09677
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-15 22:21:52 -0600

    fix bug

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -11,7 +11,7 @@ const songsReducer =()=>{
 };
 
 const selectedSongReducer=(selectedSong=null,action)=>{
-    if(action.title==='SONG_SELECTED'){
+    if(action.type==='SONG_SELECTED'){
         return action.payload;
     };
      return selectedSong;
