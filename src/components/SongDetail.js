5e8fda875dc1abaa5af9f312e71a83e1a989f763
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-10-15 23:02:10 -0600

    refactoring and building

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const SongDetail = (props)=>{
-    console.log(props)
-    return <div>Song Detail</div>
+const SongDetail = ({song})=>{
+    console.log(song);
+    if(!song){
+        return <div>Select a Song</div>
+    }
+    return <div>
+        <h3> Details For : </h3>
+        <p>
+            Title: {song.title}
+        </p> 
+        <p>
+            Duration: {song.duration}
+        </p>
+    </div>
 };
 
 const mapStateToProps = state=>{
     return {song: state.selectedSong}
 };
 
-export default connect(mapStateToProps)( SongDetail);
+export default connect(mapStateToProps)(SongDetail);
